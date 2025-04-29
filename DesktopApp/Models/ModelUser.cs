e775cd6934eb988dabba886f9d18e008e3afe03a
HenzoCH <henzodossantos7@gmail.com>
2025-04-29 11:21:21 -0300

    dsa

---
+++ DesktopApp/Models/ModelUser.cs
@@ -0,0 +1,13 @@
+namespace DesktopApp.Models;
+public class Usuario
+{
+    public string Nome { get; set; }
+    public string Email { get; set; }
+    public string Senha { get; set; }
+
+    public bool ValidarLogin(string email, string senha)
+    {
+        // Simulação de login, depois você conecta com base de dados
+        return Email == email && Senha == senha;
+    }
+}
