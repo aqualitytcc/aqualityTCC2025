635a66c4f0c173a37b448f22aea60b1967509593
Henzo dos Santos <126629602+HenzoCH@users.noreply.github.com>
2025-06-10 18:20:29 -0300

    Delete AqualityApp directory

--- AqualityApp/AqualityApp/ViewModels/MainWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace AqualityApp.ViewModels;
-
-public class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia!";
-}
--- AqualityApp/AqualityApp/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using ReactiveUI;
-
-namespace AqualityApp.ViewModels;
-
-public class ViewModelBase : ReactiveObject
-{
-}
