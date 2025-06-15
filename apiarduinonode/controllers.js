ff4b074f3da8a45b0e87950d4e9feecb0e7c75fa
henzo <henzodossantos7@gmail.com>
2025-06-15 15:56:35 -0300

    Atyakuzeu algumas coisas e integrei banco de dados, mas preciso corrigir os erros dos controladores

--- apiarduinonode/controllers.js
+++ apiarduinonode/controllers.js
@@ -1,17 +1,49 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import mysql2 from 'mysql2';
 
+const conn= mysql2.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'master',
+    database:'aquality'});
 let dispositivos=[];
-
+let usuarios=[];
+let leituras=[];
 export const obterDispositivos = (req, res) => {
+    conn.query('SELECT * FROM dispositivos', (error, dados) => {
+        if(error){
+            res.status(500).json({message: 'Erro ao obter dispositivos'});
+            return;
+        }
+        dispositivos = dados.map(d => ({
+            id: d.id,
+            nome: d.nome,
+            usuario_id: d.usuario_id,
+            descricao: d.descricao,
+            criado_em: d.criado_em
+        }));
     res.json(dispositivos);
+    });
 }
-export const adicionarDispositivo = (req,res) => {
-    const dispostivo=req.body;
-    dispositivos.push(dispostivo);
-    res.status(201).json(dispostivo);
-}
+export const adicionarDispositivo = (req, res) => {
+    const { id, usuario_id, nome, descricao, criado_em } = req.body;
+    conn.query(`INSERT INTO dispositivos VALUES (${id}, ${usuario_id}, ${nome}, ${descricao}, ${criado_em})`, (error, results) => {
+        if (error) {
+            res.status(500).json({ message: 'Erro ao adicionar dispositivo', error: error.message });
+            return;
+        }
+        res.status(201).json({
+            id: id.length + 1,
+            usuario_id,
+            nome,
+            descricao,
+            criado_em
+        });
+    });
+};
+/*
 export const atualizarDispositivo = (req, res) => {
     const id= parseInt(req.params.id);
     const dispositivo = req.body;
@@ -35,4 +67,4 @@ export const removerDispositivo=(req, res)=>{
         dispositivos.splice(index, 1);
         res.json({message: 'Dispositivo removido com sucesso'});
     }
-}
+}*/
--- apiarduinonode/rotas.js
+++ apiarduinonode/rotas.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import { obterDispositivos , adicionarDispositivo, removerDispositivo, atualizarDispositivo} from './controllers.js';
+import { obterDispositivos} from './controllers.js';
 
 const router = express.Router();
 router.use(cors());
 router.use(bodyParser.json());
 
-router.get('/dispositivos', obterDispositivos);
+router.get('/dispositivos', obterDispositivos);/*
 router.post('/dispositivos', adicionarDispositivo);
 router.put('/dispositivos/:id', atualizarDispositivo);
-router.delete('/dispositivos/:id', removerDispositivo);
+router.delete('/dispositivos/:id', removerDispositivo);*/
 export default router;
