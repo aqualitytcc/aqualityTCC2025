c893d3e95949d5c1f0f87e1a5218b2161026b4f8
aqualitytcc <aqualitytcc2025@gmail.com>
2025-10-15 23:00:44 -0300

    Clear database configuration values in config.php
    Removed sensitive database connection details.

--- servidor/config.php
+++ servidor/config.php
@@ -8,12 +8,13 @@ if (basename(__FILE__) == basename($_SERVER['SCRIPT_FILENAME'])) {
 }
 
 // Configurações do Banco de Dados
-define('DB_SERVIDOR', 'aquality_db.mysql.dbaas.com.br');
-define('DB_USUARIO', 'aquality_db');
-define('DB_SENHA', 'ROSA123456a#');
-define('DB_BANCO', 'aquality_db');
+define('DB_SERVIDOR', '');
+define('DB_USUARIO', '');
+define('DB_SENHA', '');
+define('DB_BANCO', '');
 
 // Configura o PHP para não exibir erros detalhados em produção (melhor para segurança)
 // Em fase de desenvolvimento, você pode comentar a linha abaixo para ver os erros.
 //ini_set('display_errors', 0);
+
 //error_reporting(0);
